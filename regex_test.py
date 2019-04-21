d19f2d6f7ee5e65ef6c833fecc3215f9385d4c4e
Yogendra Tamang <yogen48@hotmail.com>
2019-04-21 21:58:34 +0545

    update regexes

--- regex_test.py
+++ regex_test.py
@@ -2,6 +2,14 @@ import re
 import sys
 import json
 
+LIST_NOISES = [
+'',
+'passport no', 
+'passport no',
+'bosnia and herzegovina passport number',
+'qatari passport number',
+'jordanian passport numbers',
+]
 def extract_passport(raw_string):
     '''
     '''
@@ -12,12 +20,13 @@ def extract_passport(raw_string):
         r'Code\s+(\w+)$',
         r'(?:^|\s+)([A-Z]+\d{2,}[A-Z]+?)',
         r'\w?\d{5,}\w?',
+        r'[N|n]umber:?\s([A-Z0-9\-]+)',
             ]
     for pass_regex in pass_regexes:
         id_numbers = re.findall(pass_regex, raw_string)
         id_numbers = [_.strip() for _ in id_numbers if _.strip()]
         if id_numbers:
-            return id_numbers
+            return [id_numbers[0]]
     return [raw_string]
 
 def extract_list(raw_string):
@@ -27,12 +36,25 @@ def extract_list(raw_string):
     '''
     _list = []
     #raw_list = re.findall(r'\(\d{1,}\)[A-Za-z,\-.0-9\s]+', raw_string)
-    raw_list = re.split(r'(?:^|\s|-)\(?[\da-e]\)\s', raw_string)
+    list_regexes = [
+            r'\(\d{1,2}\)\s',
+            # . And 
+            r'\.\sAnd\s',
+            r'\([a-e]\)',
+            r'\([A-E]\)',
+            r'(?:^|\s|-)\(?[\da-e]\)\s',
+            r'Yemeni passport number',
+            ]
+    for _p in list_regexes:
+        raw_list = re.split(_p, raw_string)
+        if len(raw_list) > 1:
+            break
     if not raw_list:
+        # CAses of "4546454 and 56794730"
         raw_list.append(raw_string)
     for _item in raw_list:
         _raw = re.sub(r'\(\d{1}\)','',_item)
-        if _raw.strip() != '':
+        if _raw.lower().strip() not in LIST_NOISES:
             _list.append(_raw.strip())
     return list(_list)
 
@@ -42,28 +64,55 @@ def extract_date(raw_string, mode=None):
     date_regexes = [
             # issued on 16 Nov 1989
             r'issued\s?(?:on)?\s+(\d{1,2}\s+(?:\w+)\s+\d{1,4})',
-            r'[I|i]ssued\s?(?:on)?\s?(\d{1,2}\s+(?:\w+)\s+\d{1,4})',
-            r'[I|i]ssued\s?(?:on)?\s?(\d{1,2}[./-]+\d{1,2}[./-]+\d{4})'
-            r'[I|i]ssued\s?(?:on)?\s?(\d{1,2}[./-]+\d{1,2}[./-]+\d{4})'
+            r'[I|i]ssued\s?(?:on)?\s?(\d{1,2}\s+[A-Za-z\.]+\s+\d{1,4})',
+            r'[I|i]ssued:?\s?(?:on)?\s?(\d{1,2}[./-]+\d{1,2}[./-]+\d{4})',
             # issued on 16 9.12.2003
+            # date of issue: 24.5.2015
+            r'issue:\s?(\d{1,2}\.\d{1,2}\.\d{4})',
+            # date of issue 24 November 2015
+            r'issue:?\s?(\d{1,2}\s[A-Za-z\.]+\s+\d{4})',
+            # valid 21/03/2016
+            # valid 13.11.2013
+            r'valid\s?(\d{1,2}[./-]+\d{1,2}[./-]+\d{4})',
+            # valid from 15.4.2011 until 14.4.2016
+            r'valid\sfrom\s?(\d{1,2}[./-]+\d{1,2}[./-]+\d{4})',
+            # (approximately issued in 2009)
+            r'approximately issued in\s(\d{4})',
                 ]
     if mode == "exp":
         date_regexes = [
                 # issued on 16 Nov 1989
                 r'[V|v]alid\s?(?:[U|u]ntil)?\s+(\d{1,2}\s+(?:\w+)\s+\d{1,4})',
-                r'[E|i]xpir[eds]+\s?(?:on)?\s?(\d{1,2}\s+(?:\w+)\s+\d{1,4})',
-                r'[E|i]xpir[eds]+\s?(?:on)?\s?(\d{1,2}[/-]+\d{1,2}[/-]+\d{4})',
+                # expires on 6 june 2013
+                r'[E|e]xpir[edsing]+\s?(?:on)?\s?(\d{1,2}[-/\s]+[A-Za-z\.]+[-/\s]+\d{4})',
+                # expiration date: 3.6.2016
+                r'[E|e]xpiration:?\s(?:date)?:?\s?(?:on)?\s?(\d{1,2}[-/\s\.]\d{1,2}[-/\s\.]+\d{4})',
+                # expiration date 25 Feb. 2026
+                r'[E|e]xpiration (?:date)?:?\s?(?:on)?\s?(\d{1,2}[-/\s]+[A-Za-z\.]+[-/\s]+\d{4})',
+                # expired 23.09.2023
+                # expires 23.09.2023
+                r'[E|e]xpir[eds]+:?\s?(?:on)?\s?(\d{1,2}[-/\.]+\d{1,2}[-/\.]+\d{4})',
+                r'[E|e]xpir[eds]+\s?(?:on)?\s?(\d{1,2}\s+(?:\w+)\s+\d{1,4})',
+                # date of expiry: 23.5.2021
+                # expiring 03/05/2022
+                r'expir[ingy]+:?\s?(\d{1,2}[\./]+\d{1,2}[\./]+\d{4})',
+                # date of expiry 24 November 2015
+                r'expiry:?\s?(\d{1,2}\s[A-Za-z\.]+\s+\d{4})',
+                # expiry date of 24 November 2015
+                r'expiry date (?:of)?\s?(\d{1,2}\s[A-Za-z\.]+\s+\d{4})',
+                # valid from 15.4.2011 to 14.4.2016
+                r'valid\sfrom\s?\d{1,2}[./-]+\d{1,2}[./-]+\d{4}\s+to\s+(\d{1,2}[./-]+\d{1,2}[./-]+\d{4})',
+                # valid until 4.2.2019.
+                r'until\s?(\d{1,2}\.\d{1,2}\.\d{4})',
+                # expires 23.09.2023
+                # expiry date
+                # Date of Expiration: 6.4.2016
                     ]
     _date = None
     for pattern in date_regexes:
         pattern_out = re.findall(pattern, raw_string)
         if pattern_out:
-            try:
-                _date = nlp.resolve_date_range(pattern_out[0])
-                return _date
-                break
-            except:
-                pass
+            return pattern_out
     return _date
 
 
@@ -94,12 +143,10 @@ def check_format(mode):
 if __name__ == '__main__':
     '''
     '''
-    counter = 1
     for _id in check_format(sys.argv[1]):
-        print("====== ", counter)
+        print("====== ", (_id['index']+1))
         jdata = json.dumps(_id, ensure_ascii=False, indent=2)
         print(jdata)
-        counter += 1
 
 
 
